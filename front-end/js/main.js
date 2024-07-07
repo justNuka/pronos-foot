ca6363f9812a2155c6a067ec017eed1d63e87ae5
justNuka <75932540+justNuka@users.noreply.github.com>
2024-07-07 17:02:50 +0200

    OK sans styyle

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -26,6 +26,14 @@ function formatDate(dateString, timeString) {
     return `${capitalizedDate} à ${formattedTime}`;
 }
 
+// Fonction pour exécuter les scripts injectés (les scripts sont sinon bloqués sur la page index.html par le navigateur, pas le cas sur admin.html)
+function executeScripts(element) {
+    const scripts = element.getElementsByTagName('script');
+    for (let i = 0; i < scripts.length; i++) {
+        eval(scripts[i].innerText); // Note: 'eval' est utilisé ici uniquement pour démontrer une vulnérabilité XSS. Ne jamais utiliser 'eval' dans du code sécurisé.
+    }
+}
+
 // Charge les prédictions depuis l'API et les affiche sur la page d'accueil
 function loadPredictions() {
     fetch('http://localhost:3000/api/predictions')
@@ -36,9 +44,19 @@ function loadPredictions() {
             predictions.forEach(prediction => {
                 const div = document.createElement('div');
                 const formattedDateTime = formatDate(prediction.date, prediction.time);
-                // Utilisation de innerHTML pour démontrer XSS
-                div.innerHTML = `${prediction.team1} vs ${prediction.team2} - Prédiction : ${prediction.prediction} le ${formattedDateTime}`;
+
+                // Inner HTML vulnérable à XSS
+                div.innerHTML = `
+                    <div class="prediction">
+                        <p>Team 1: ${prediction.team1}</p>
+                        <p>Team 2: ${prediction.team2}</p>
+                        <p>Prediction: ${prediction.prediction}</p>
+                        <p>Date: ${formattedDateTime}</p>
+                        <p>Stage: ${prediction.stage}</p>
+                    </div>
+                `;
                 predictionsDiv.appendChild(div);
+                executeScripts(div); // Exécute les scripts injectés -- vulnérabilité XSS à ne pas mettre en prod
             });
         })
         .catch(error => {
